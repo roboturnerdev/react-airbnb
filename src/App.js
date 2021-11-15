525130fa2d941de89a7393f48c83b354cf6c3ec5
Robert Turner <roboturnerdev@gmail.com>
2021-11-15 15:06:59 -0500

    sending props with great vigor

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
 export default function App() {
       // <Hero />
   return (
     <div className="app">
       <Navbar />
-      <Card />
+      <Card 
+        img="katie.png"
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import katie from "../images/katie.png";
 import star from "../images/star1.png";
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="card">
-            <img src={katie} alt="" className="card--image" />
+            <img src={`../images/${props.img}`} alt="" className="card--image" />
             <div className="card--stats">
                 <img src={star} alt="" className="card--star" />
-                    <span>5.0</span>
-                    <span className="gray">(6) •</span>
-                    <span className="gray">USA</span>
+                    <span>{props.rating}</span>
+                    <span className="gray">({props.reviewCount}) •</span>
+                    <span className="gray">props.country</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
